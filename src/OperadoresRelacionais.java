33115c556d8111e00d0d4a61da2ad2b0a0a65168
Lucas Oliveira <rm568356@fiap.com.br>
2026-02-12 20:17:10 -0300

    Estudos dia 11.02.2026 - 2h30min

--- src/OperadoresRelacionais.java
+++ src/OperadoresRelacionais.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class OperadoresRelacionais {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        //Criando as variáveis:
+        boolean ensolarado;
+        boolean finalDeSemana;
+
+        //Solicitando informações ao usuário:
+        System.out.println("O dia está ensolarado hoje? (true para sim / false para não) ");
+        ensolarado = sc.nextBoolean();
+
+        System.out.println("Hoje é final de semana? (true para sim / false para não) ");
+        finalDeSemana = sc.nextBoolean();
+
+        //Verificar informações para decidir onde ir:
+        if (ensolarado && finalDeSemana) {
+            System.out.println("Vamos para a praia, aproveitar o sol no final de semana");
+        }
+        else {
+            System.out.println("Hoje o passeio será no parque!");
+        }
+    }
+
 }
--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -1,4 +1,53 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class SwitchCase {
-}
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        //Declarar variáveis:
+        int opcao;
+        double numero1;
+        double numero2;
+        double resultado;
+
+        //Solicitar informações:
+        System.out.println("Bem-vindo à Calculadora!");
+        System.out.println("Por favor, escolha uma opção: ");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
+
+        //Lendo as informações:
+        opcao = sc.nextInt();
+        System.out.println("Digite dois números: ");
+        numero1 = sc.nextDouble();
+        numero2 = sc.nextDouble();
+
+        //Switch Case:
+        switch (opcao){
+            case 1:
+                resultado = numero1 + numero2;
+                System.out.println("Resultado da soma: " + resultado);
+                break;
+            case 2:
+                resultado = numero1 - numero2;
+                System.out.println("Resultado da subtração: " + resultado);
+                break;
+            case 3:
+                resultado = numero1 * numero2;
+                System.out.println("Resultado da multiplicação: " + resultado);
+                break;
+            case 4:
+                if (numero2 != 0) {
+                    resultado = numero1 / numero2;
+                    System.out.println("O resultado da divisão é: " + resultado);
+                } else {
+                    System.out.println("Não é possível dividir por zero!");
+                }
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                }
+        }
+    }
